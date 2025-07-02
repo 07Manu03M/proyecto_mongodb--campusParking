47f63465eaa0eb6cadd8b9454b74a6db77741de6
manuel meneses <manuelmeneses@gmail.com>
2025-07-02 12:27:39 -0500

    feat: :sparkles: agregaciones
    se han añadido las agregaciones 7/8

--- trasactions.js
+++ trasactions.js
@@ -1,1 +1,21 @@
-// Transaccion funcional entre colecciones
+const session = db.getMongo().startSession();
+const dbSession = session.getDatabase("test");
+
+session.startTransaction();
+try {
+  dbSession.usuarios.insertOne({
+    _id: 11,
+    clienteId: 11,
+    productos: [{productoId: 1, cantidad: 2}],
+    fecha: ISODate("2025-06-22"),
+    total: 10000
+  });
+  dbSession.productos.updateOne({_id: 1}, {$inc: {stock: -2}});
+  session.commitTransaction();
+  print("Venta confirmada");
+} catch (error) {
+  session.abortTransaction();
+  print("Error:", error);
+} finally {
+  session.endSession();
+}
